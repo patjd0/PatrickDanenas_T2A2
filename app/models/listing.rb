c37351b317c4af3ce4863a4887812aaebb6a627c
patjd0 <pjdnas@gmail.com>
2022-07-17 23:06:00 +1000

    drop down category and botton conditions

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    if_a_mug_could_rust: 1,
+    been_round_the_block: 2,
+    lightly_sipped: 3,
+    brand_new: 4
+  }
 end
