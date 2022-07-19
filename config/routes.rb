3a01a2fbef3837771c7df6b9da4918a635676134
patjd0 <pjdnas@gmail.com>
2022-07-19 16:00:48 +1000

    buy button

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 end
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "listings/:id/order", to: "listings#place_order", as: "place_order"
+  get 'pages/success', to: 'pages#success', as: "order_success"
 end
