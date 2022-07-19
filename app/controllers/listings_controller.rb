78fa4be4fed44289c848f716ab70ecc8cf455a14
patjd0 <pjdnas@gmail.com>
2022-07-19 18:51:00 +1000

    success page update

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -69,7 +69,7 @@ class ListingsController < ApplicationController
     )
     @listing.update(sold: true)
 
-    redirect_to order_success_path
+    redirect_to orders_success_path
   end  
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'orders/success'
   get 'orders/bought'
   get 'orders/sold'
   resources :listings
@@ -10,5 +9,5 @@ end
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "listings/:id/order", to: "listings#place_order", as: "place_order"
-  get 'pages/success', to: 'pages#success', as: "order_success"
+  get 'orders/success'
 end
