bd9aa176ba0066a921a3ef45ec64e83b33c5e7bb
patjd0 <pjdnas@gmail.com>
2022-07-19 17:04:15 +1000

    listing details/sold/ownListing/notSignedin

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -67,7 +67,9 @@ class ListingsController < ApplicationController
       seller_id: @listing.user_id,
       buyer_id: current_user.id
     )
-  redirect_to order_success_path
+    @listing.update(sold: true)
+
+    redirect_to order_success_path
   end  
 
   private
